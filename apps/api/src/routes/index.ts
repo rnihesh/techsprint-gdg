2c0a76eabfed534218d989531b0f1f7d15708906
kmahesh18 <maheshkolli888@gmail.com>
2025-12-31 19:12:34 +0530

    rm

--- apps/api/src/routes/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as issueRoutes } from "./issues.js";
-export { default as municipalityRoutes } from "./municipality.js";
-export { default as leaderboardRoutes } from "./leaderboard.js";
-export { default as maintainerRoutes } from "./maintainer.js";
-export { default as metaRoutes } from "./meta.js";
